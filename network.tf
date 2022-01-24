feba65fdcb8542b3d7d98d89dfa9d26b06fbe209
avoidik <avoidik@gmail.com>
2022-01-24 18:34:05 +0000

    use specific rg name for network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/graceful-hippo/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "${var.prefix}-net"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
